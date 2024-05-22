a5a3a0ffbdbba5c5bc35372fb493b8d99691949e
surajmaji10 <akashmaji728@gmail.com>
2024-05-22 15:25:55 +0530

    lottery

---
+++ src/main/java/com/akash/arrays/Arrays.java
@@ -0,0 +1,8 @@
+package com.akash.arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] lotties = {1, 2, 3, 4, 5};
+        System.out.println(java.util.Arrays.toString(lotties));
+    }
+}
---
+++ src/main/java/com/akash/arrays/LotteryTicket.java
@@ -0,0 +1,51 @@
+package com.akash.arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class LotteryTicket {
+    private static final int LENGTH = 6;
+    private static final int SIZE = 100;
+
+    public static void main(String[] args) {
+        System.out.println("Hello! Welcome to the lottery game!");
+
+        int[] lottery = generateLottery(LENGTH, SIZE);
+
+        //System.out.println(Arrays.toString(lottery));
+        System.out.println("The generated lottery is: ");
+        showLottery(lottery);
+    }
+
+    public static int[] generateLottery(int length, int size){
+        int[] lottery = new int[length];
+        Random random = new Random();
+        for(int i = 0; i < length; i++){
+            int randInt = random.nextInt(size);
+            while(containsAlready(lottery, randInt)){
+                randInt = random.nextInt(size);
+            }
+            lottery[i] = randInt;
+        }
+        return lottery;
+    }
+
+    public static boolean containsAlready(int[] lottery, int value){
+        for(int v: lottery){
+            if(v == value) return true;
+        }
+        return false;
+    }
+
+    public static void showLottery(int[] lottery){
+        System.out.print("| ");
+        for(int v: lottery){
+            if(v < 10){
+                System.out.print("0"+v + " | ");
+            }else{
+                System.out.print(v + " | ");
+            }
+        }
+    }
+
+}
